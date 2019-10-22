afa0ee0c8539ae2781378859a5751dbdfff50cc7
sandeep <sandeep@ashmar.in>
2019-10-22 16:55:21 +0530

    add

--- app/src/main/java/com/example/videoplayerrecycler/MainActivity.java
+++ app/src/main/java/com/example/videoplayerrecycler/MainActivity.java
@@ -6,6 +6,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.widget.LinearLayout;
+import android.widget.MediaController;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -13,24 +14,21 @@ import java.util.Arrays;
 public class MainActivity extends AppCompatActivity {
     RecyclerView recyclerView;
     ArrayList personNames;
-
+    String url []={"https://videocdn.bodybuilding.com/video/mp4/62000/62792m.mp4","https://clips.vorwaerts-gmbh.de/VfE_html5.mp4","https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerFun.mp4","https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerFun.mp4","https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerEscapes.mp4"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         recyclerView = findViewById(R.id.rv);
-        personNames.add("gjhkh");
-        personNames.add("ghjkhk");
-        personNames.add("eyttu");
-        personNames.add("sfff");
-        personNames.add("oppo");
-        personNames.add("fygjhk");
-        personNames.add("gjhk");
-        personNames.add("cghvh");
-        MyAdapter myAdapter = new MyAdapter(personNames, this);
+        personNames= new ArrayList();
+      //  MediaController midiacontrtoler = new MediaController(this);
+
+        MyAdapter myAdapter = new MyAdapter(url,this);
         recyclerView.setAdapter(myAdapter);
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(linearLayoutManager);
 
+
     }
+
 }
--- app/src/main/java/com/example/videoplayerrecycler/MyAdapter.java
+++ app/src/main/java/com/example/videoplayerrecycler/MyAdapter.java
@@ -1,10 +1,16 @@
 package com.example.videoplayerrecycler;
 
 import android.content.Context;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.MediaController;
 import android.widget.TextView;
+import android.widget.VideoView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -12,38 +18,60 @@ import androidx.recyclerview.widget.RecyclerView;
 import java.util.ArrayList;
 
 public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyHolder> {
-    ArrayList Al;
+    String[] Al;
     Context context;
 
-    public MyAdapter(ArrayList al, Context context) {
+    public MyAdapter(String[] al, Context context) {
         Al = al;
         this.context = context;
+
     }
 
     @NonNull
     @Override
     public MyHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.items,parent,false);
-         MyHolder myHolder=new MyHolder(view);
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.items, parent, false);
+        MyHolder myHolder = new MyHolder(view);
         return myHolder;
     }
 
     @Override
-    public void onBindViewHolder(@NonNull MyHolder holder, int position) {
-        holder.textView.setText(""+Al.get(position));
-
+    public void onBindViewHolder(@NonNull final MyHolder holder, int position) {
+        holder.textView.setText(Al[position]);
+        String url = Al[position];
+        Log.d("TAg", "url=" + Al[position]);
+        Uri uri = Uri.parse(url);
+        holder.videoView.setVideoURI(uri);
+      //  holder.videoView.start();
+        holder.button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                holder.videoView.pause();
+            }
+        });
+        holder.button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                holder.videoView.start();
+            }
+        });
     }
 
     @Override
     public int getItemCount() {
-        return Al.size();
+        return Al.length;
     }
 
     public class MyHolder extends RecyclerView.ViewHolder {
         TextView textView;
+        VideoView videoView;
+        Button button,button2;
         public MyHolder(@NonNull View itemView) {
             super(itemView);
-            textView=itemView.findViewById(R.id.t1);
+            textView = itemView.findViewById(R.id.t1);
+            videoView = itemView.findViewById(R.id.video);
+            button = itemView.findViewById(R.id.b1);
+            button2=itemView.findViewById(R.id.b2);
         }
     }
 }
